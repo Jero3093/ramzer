2eef764e503ba0b86c8a34a4f71070205acb7d98
Jeronimo <jeronimotornini@gmail.com>
2024-12-15 13:33:11 -0300

    Update styles on home page and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import TypeButton from "@/components/TypeButton";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center text-pretty gap-8 pt-10 md:pt-20">
+    <main className="min-h-screen flex flex-col items-center text-pretty gap-8 pt-10">
       <Header />
       <h2 className="text-zinc-400 text-xl">
         ¡Choose whatever you want to watch!
       </h2>
-      <section className="flex flex-col md:flex-row gap-8 items-center mt-5">
+      <section className="w-full flex flex-col md:flex-row gap-8 items-center justify-center mt-5 md:gap-20">
         <TypeButton href={"/tv"} title={"TV Shows"} color="#ef4444" />
         <TypeButton href={"/movies"} title={"Movies"} color="#0ea5e9 " />
       </section>
-      <footer className="mt-auto mb-10">
-        Powered by <strong className="text-sky-300">TMDB</strong> Api
-      </footer>
+      <Footer />
     </main>
   );
 }
--- src/components/TypeButton.tsx
+++ src/components/TypeButton.tsx
@@ -5,11 +5,11 @@ function TypeButton({ href, title, color }: TypeButtonInt) {
   return (
     <Link
       href={href}
-      className="w-52 h-52 rounded-full border-4 border-red-500 grid place-items-center hover:scale-110 transition-all"
+      className="w-52 h-52 rounded-full border-4 border-red-500 grid place-items-center md:w-80 md:h-80 md:border-8 hover:scale-110 transition-all"
       style={{ borderColor: color }}
     >
       <div
-        className="w-48 h-48 bg-red-500 rounded-full grid place-items-center"
+        className="w-48 h-48 bg-red-500 rounded-full grid place-items-center md:w-72 md:h-72"
         style={{ background: color }}
       >
         <h3 className="font-semibold text-3xl">{title}</h3>
