3e38dfbdb3da9281cb4c1ea970f8df824df0af6e
Jeronimo <jeronimotornini@gmail.com>
2024-12-18 18:38:01 -0300

    Include watchlist link button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,10 @@ export default function Home() {
       <h2 className="text-zinc-400 text-xl">
         ¡Choose whatever you want to watch!
       </h2>
-      <section className="w-full flex flex-col md:flex-row gap-8 items-center justify-center mt-5 md:gap-20">
+      <section className="w-full flex flex-col md:flex-row gap-8 items-center justify-center flex-wrap mt-5 md:gap-12">
         <TypeButton href={"/tv"} title={"TV Shows"} color="#ef4444" />
-        <TypeButton href={"/movies"} title={"Movies"} color="#0ea5e9 " />
+        <TypeButton href={"/movies"} title={"Movies"} color="#0ea5e9" />
+        <TypeButton href={"/watchlist"} title={"Watchlist"} color="#52525b" />
       </section>
       <Footer />
     </main>
